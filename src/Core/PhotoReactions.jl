64d53238525a8766af565340a8a86a920e961b02
jonathanBalasingham <jonbalasin@gmail.com>
2021-06-07 13:08:33 +0100

    add functions to photoreactions

--- src/Core/PhotoReactions.jl
+++ src/Core/PhotoReactions.jl
@@ -47,4 +47,145 @@ function H2PhotoDissRate(NH2, radField, av, turbVel)
     baseRate * (radField/1.7) * scatter(xl, av) * H2SelfShielding(NH2, dopplerWidth, radWidth)
 end
 
+function COPhotoDissRate(NH2,NCO,radField,av)
+    ssf = COSelfShielding(NH2, NCO)
+    lba = lbar(NCO, NH2)
+    sca = scatter(lba, av)
+
+    # The reason why rad is divided by 1.7 is that the alphas are for Draine and the rad is in 
+    # Habing units
+    COPhotoDissRate = (2.0e-10) * (radfield/1.7) * ssf * sca
+end
+
+function cIonizationRate(alpha,gamma,gasTemp,NC,NH2,av,radfield)
+    #  Calculate the optical depth in the CI absorption band, accounting
+    #  for grain extinction and shielding by CI and overlapping H2 lines
+    TAUC = gamma*av+1.1e-17*NC+(0.9*gasTemp^0.27*(NH2/1.59e21)^0.45)
+    #  Calculate the CI photoionization rate
+    RATE = alpha*(radfield/1.7)*exp(-TAUC)
+end
+
+function H2SelfShielding(NH2,dopplerWidth,radWidth)
+    FPARA = 0.5
+    FOSC = 1.0e-2
+    taud  = FPARA * NH2 * 1.5e-2 * FOSC / dopplerWidth
+     
+    # calculate doppler contribution of self shielding function sj
+    if (taud .eq. 0.0) 
+       sj = 1.0
+    elseif (taud .lt. 2.0) 
+       sj = exp(-0.6666667*taud)
+    elseif (taud .lt. 10.0) 
+       sj = 0.638*taud^(-1.25)
+    elseif (taud .lt. 100.0) 
+       sj = 0.505*taud^(-1.15)
+    else
+       sj = 0.344*taud^(-1.0667)
+    end
+
+    # calculate wing contribution of self shielding function sr
+    # if (taud.lt.0.0) taud=0.0 end
+    if (radWidth .eq. 0.0)
+       sr = 0.0
+    else
+       r  = radWidth/(1.7724539*dopplerWidth)
+       t  = 3.02 * ((r*1.0d+03)^(-0.064))
+       u  = sqrt(taud*r)/t
+       sr = r/(t*sqrt(0.78539816+u^2.0))
+    end
+    
+    # calculate total self shielding function fgk
+    H2SelfShielding = sj + sr
+end
+
+function scatter(x1, av)
+    c = [1.0, 2.006, -1.438, 7.364e-01, -5.076e-01, -5.920e-02]
+    k1 = [7.514e-01, 8.490e-01, 1.013, 1.282, 2.005, 5.832]
+
+    # optical depth in the visual
+    tv = av/ 1.086
+      
+    # make correction for the wavelength considered
+    tl = tv * xlambda(x1)
+       
+    # calculate attuentuation  due to dust scattering
+    scatter = 0.0
+    if tl < 1.0
+        expo = k1[1] * tl
+        if expo < 100.0 
+            scatter = c[1] * exp(-expo)
+        end
+    else
+        for i in 2:6
+            expo = k1[i]*tl
+            if expo < 100.0 
+                scatter += c[i]*exp(-expo)
+            end
+        end
+    end
+    return scatter
+end
+
+function xlambda(LAMBDA)
+
+    if START
+        SPLINE(LAMBDA_GRID, XLAMBDA_GRID, NUM_LAMBDA,1.0e30,1.0e30, XLAMBDA_DERIV)
+    end
+
+    LAMBDA_VALUE = LAMBDA
+    if LAMBDA < LAMBDA_GRID[1] LAMBDA_VALUE = LAMBDA_GRID[1] end
+    if LAMBDA > LAMBDA_GRID[NUM_LAMBDA] LAMBDA_VALUE = LAMBDA_GRID[NUM_LAMBDA] end
+
+    SPLINT(LAMBDA_GRID,XLAMBDA_GRID,XLAMBDA_DERIV,NUM_LAMBDA,LAMBDA_VALUE,xlambda)
+    if XLAMBDA < 0.0 XLAMBDA = 0.0 end
+
+end
+
+function COSelfShielding(NH2,NCO)
+    if startr
+        splie2(NCO_GRID,NH2_GRID,SCO_GRID,DIMCO,DIMH2,SCO_DERIV)
+        startr = false
+    end
+   
+    lognco = log10(NCO+1.0)
+    lognh2 = log10(NH2+1.0)
+       
+    if lognco < NCO_GRID[1]      lognco = NCO_GRID[1] end
+    if lognh2 < NH2_GRID[1]      lognh2 = NH2_GRID[1] end
+    if lognco > NCO_GRID[DIMCO]  lognco = NCO_GRID[DIMCO] end
+    if lognh2 > NH2_GRID[DIMH2]  lognh2 = NH2_GRID[DIMH2] end
+       
+    splin2(NCO_GRID,NH2_GRID,SCO_GRID,SCO_DERIV,DIMCO,DIMH2,lognco,lognh2,COSelfShielding)
+    COSelfShielding = 10.0^COSelfShielding
+end
+
+function lbar(u,w)
+    # calculate lambda bar (in a) according to equ. 4 of van dishoeck
+    # and black, apj 334, p771 (1988)
+    #  --------------------------------------------------------------
+    #        i/o parameter
+    #        u : co column density in (cm-2)
+    #        w : h2 column density in (cm-2)
+    
+    #         program variables
+    #         lu : log10(co column density in cm-2)
+    #         lw : log10(h2 column density in cm-2)
+    
+    # --------------------------------------------------------------
+    lu = log10(abs(u)+1.0)
+    lw = log10(abs(w)+1.0)
+    
+    lbar = (5675.0 - 200.6*lw) - (571.6 - 24.09*lw)*lu + (18.22 - 0.7664*lw)*lu^2
+       
+    # lbar represents the mean of the wavelengths of the 33
+    # dissociating bands weighted by their fractional contribution
+    # to the total rate of each depth. lbar cannot be larger than
+    # the wavelength of band 33 (1076.1a) and not be smaller than
+    # the wavelength of band 1 (913.6a).
+    if lbar > 1076.1  lbar = 1076.1 end
+    if lbar < 913.6  lbar =  913.6 end
+end
+
+
+
 end
